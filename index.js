ab3836c5fe58054c479ee265c6784a3124efca29
Sharon Moegi <sharamoegi@gmail.com>
2026-04-13 16:13:10 +0300

    Return object

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ function combineUsers(...args){
   //Initialize the return object
   let combinedObject = {
     users: []
-  }
+  };
   //Loop through args and merge arrays
   for (let i = 0; i < args.length; i++){
     let currentArray = args[i];
-    consoele.log(`Array at index ${i} : ${currentArray}`);
-    combinedObject.users = [...combinedObject.users, ...currentArray]
+    // console.log(`Array at index ${i} : ${currentArray}`);
+    combinedObject.users = [...combinedObject.users, ...currentArray];
   
-  }
+  };
   //Get today's date
   combinedObject.merge_date = new Date().toString("M/d/yyyy");
 
+  return combinedObject;
 
-}
+
+};
+
 
 
 
