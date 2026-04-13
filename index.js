b192946c0adac2da17dcf5a311e073c79bced31f
Sharon Moegi <sharamoegi@gmail.com>
2026-04-13 15:03:33 +0300

    Loop through args

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // This is a function that merges all usernames into one
 function combineUsers(...args){
+  //Initialize the return object
   let combinedObject = {
     users: []
   }
-  
+  //Loop through args
+  for (let i = 0; i < args.length; i++){
+    let currentArray = args[i];
+    consoele.log(`Array at index ${i} : ${currentArray}`);
+  }
   
 
 
